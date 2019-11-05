58dacee6e925ee34304868ff381467ee4351d792
kjuulh <hermansendev@gmail.com>
2019-11-05 14:25:31 +0100

    Fixed workout

--- src/routes/workouts.server.route.ts
+++ src/routes/workouts.server.route.ts
@@ -3,10 +3,10 @@ import { workoutsController } from '../controllers/workout.server.controller'
 
 export default class WorkoutRoute {
   constructor(app: Express) {
-    app.route('/workouts').get(workoutsController.index)
-    app.route('/workouts/mine').get(workoutsController.myWorkouts)
     app.route('/workouts').post(workoutsController.createWorkout)
-    app.route('/workouts/:workoutId').get(workoutsController.getWorkout)
-    app.route('/workouts/:workoutId/put').post(workoutsController.changeWorkout)
+    app
+      .route('/workouts/:workoutId')
+      .get(workoutsController.getWorkout)
+      .put(workoutsController.changeWorkout)
   }
 }

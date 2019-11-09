a45b23a1328aef7e5468be1a5c285cba4574c1c3
kjuulh <hermansendev@gmail.com>
2019-11-10 00:00:23 +0100

    Add cors

--- src/routes/auth.server.route.ts
+++ src/routes/auth.server.route.ts
@@ -1,12 +1,13 @@
 import { Express } from 'express'
 import passport = require('passport')
 import { authController } from '../controllers/auth.server.controller'
+import * as cors from 'cors'
 
 export default class AuthRoutes {
   constructor(app: Express) {
-    app.route('/authenticate').post(authController.authenticate)
-    app.route('/register').post(authController.register)
-    app.route('/users').get(authController.getAll)
-    app.route('/users/id/:userId').get(authController.getById)
+    app.route('/authenticate').post(cors(), authController.authenticate)
+    app.route('/register').post(cors(), authController.register)
+    app.route('/users').get(cors(), authController.getAll)
+    app.route('/users/id/:userId').get(cors(), authController.getById)
   }
 }

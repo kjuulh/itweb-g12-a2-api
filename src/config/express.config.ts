d0be0ea01611c8c86e00ce853f3e35b8bfef93d7
kjuulh <hermansendev@gmail.com>
2019-11-09 23:45:11 +0100

    Try again

--- src/config/express.config.ts
+++ src/config/express.config.ts
@@ -7,10 +7,6 @@ import routesConfig from './routes.config'
 export default function() {
   const app: express.Express = express()
 
-  databaseConfig(app)
-  parserConfig(app)
-  routesConfig(app)
-
   const originsWhitelist = [
     'http://localhost:4200',
     'https://itweb-g12-a2-app.herokuapp.com',
@@ -18,13 +14,17 @@ export default function() {
 
   const corsOptions = {
     origin: (origin, callback) => {
-      let isWhitelisted = originsWhitelist.indexOf(origin) !== -1
+      const isWhitelisted = originsWhitelist.indexOf(origin) !== -1
       callback(null, isWhitelisted)
     },
     credentials: true,
   }
 
   app.use(cors(corsOptions))
+
+  databaseConfig(app)
+  parserConfig(app)
+  routesConfig(app)
 
   return app
 }
